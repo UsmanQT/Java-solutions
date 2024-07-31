381b4092e5e52ff1f7e1075029a313bc003b49ea
usmanqt <qureshiu@mail.gvsu.edu>
2024-07-31 14:05:35 -0400

    a better efficient approach for merging two arrays alternatively

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,24 @@
 public class Solution {
     public String mergeAlternately(String word1, String word2) {
-        char[] newString = new char[word1.length() + word2.length()];
-        int j = 0;
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
 
-        for(int i = 0 ; i <= newString.length - 1 ; i++) {
-            if(newString[i] != 0) {
-                continue;
-            }
-            if(word1.length() == word2.length()) {
-                newString[i] = word1.charAt(j);
-                newString[i+1] = word2.charAt(j);
-            }
-            if(word1.length() < word2.length()) {
-                if(j < word1.length() && j < word2.length()) {
-                    newString[i] = word1.charAt(j);
-                    newString[i+1] = word2.charAt(j);
-                }
-                else {
-                    newString[i] = word2.charAt(j);
-                }
-            }
-            if(word1.length() > word2.length()) {
-                if(j < word1.length() && j < word2.length()) {
-                    newString[i] = word1.charAt(j);
-                    newString[i+1] = word2.charAt(j);
-                }
-                else {
-                    newString[i] = word1.charAt(j);
-                }
-            }
-
-            j++;
+        // Merge characters alternately
+        while (i < word1.length() && j < word2.length()) {
+            result.append(word1.charAt(i++));
+            result.append(word2.charAt(j++));
         }
 
-        String a = new String(newString);
-        return a;
-    }
+        // Append remaining characters from word1, if any
+        while (i < word1.length()) {
+            result.append(word1.charAt(i++));
+        }
 
+        // Append remaining characters from word2, if any
+        while (j < word2.length()) {
+            result.append(word2.charAt(j++));
+        }
+
+        return result.toString();
+    }
 }
